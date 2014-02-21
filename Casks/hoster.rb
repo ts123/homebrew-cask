467504d40653be856416e19c12d4bd25c0745b5f
ts123 <hukurou@gmail.com>
2014-02-21 20:01:24 +0900

    add hoster

---
+++ Casks/hoster.rb
@@ -0,0 +1,7 @@
+class Hoster < Cask
+  url 'http://www.redwinder.com/macapp/hoster/tmp/Hoster1.208.zip'
+  homepage 'http://www.redwinder.com/macapp/hoster/'
+  version '1.208'
+  md5 '5d5bfb8d34b38338c3f7f4a6e40c8997'
+  install 'Hoster1.208.mpkg'
+end
