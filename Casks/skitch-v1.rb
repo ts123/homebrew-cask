29c275c56508000d55ee9d305299f192ac6a6316
ts123 <hukurou@gmail.com>
2014-02-21 19:38:08 +0900

    fix skitch

--- Casks/skitch-v1.rb
+++ Casks/skitch-v1.rb
@@ -1,7 +1,7 @@
-class Skitch1 < Cask
+class SkitchV1 < Cask
   url 'http://download.oldapps.com/skitch/Skitch-1.0.12.zip'
   homepage 'http://www.oldapps.com/mac/skitch_for_mac.php?old_skitch=10010'
   version '1.0.12'
-  sha1 'C68EA82B79BFCCAEE3CB6CC2B40C4C3898375E0D'
+  sha1 'c68ea82b79bfccaee3cb6cc2b40c4c3898375e0d'
   link 'Skitch.app'
 end
