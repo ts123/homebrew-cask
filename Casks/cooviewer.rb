5cd2446beb7fd60c210207b2e7723901d411b94c
ts123 <hukurou@gmail.com>
2014-02-21 15:19:01 +0900

    fix cooviewr

--- Casks/cooviewer.rb
+++ Casks/cooviewer.rb
@@ -3,5 +3,5 @@ class Cooviewer < Cask
   homepage 'http://www.geocities.jp/coo_ona/viewer.html'
   version '1.2b24'
   sha1 'fadae4e90080cff93b1c07342824c495dd14dd01'
-  link 'cooViewer.app'
+  link 'cooViewer/cooViewer.app'
 end
